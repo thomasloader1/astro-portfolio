e20289d3db4cd5a7390f9a76bddd96531f5a15df
Tomas <talk.gtg@gmail.com>
2024-01-08 15:44:56 -0300

    Fix mobile

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -17,8 +17,14 @@ export default function ThemeToggle() {
   }, [theme]);
 
   return (
-    <div className="border-s px-2 ml-2">
-      <button onClick={handleClick} className="transition p-2">{theme === "light" ? <img src="/Moon.svg" alt="" />: <img src="/Sun.svg" alt="" />}</button>
+    <div className="border-t mt-2 md:mt-0 md:border-t-transparent md:border-s px-2 ml-2">
+      <button onClick={handleClick} className="transition p-2">{theme === "light" ? (<div className="flex items-center text-black dark:text-white">
+        <img src="/Moon.svg" alt="" />
+        <span className="ml-2 md:hidden">Oscuro</span>
+        </div>): (<div className="flex items-center text-black dark:text-white">
+        <img src="/Sun.svg" alt="" />
+        <span className="ml-2 md:hidden">Claro</span>
+        </div>)}</button>
     </div>
   );
 }
