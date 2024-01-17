c9ea169abb3f7f08bb929904c2ca774592549035
Tomas <talk.gtg@gmail.com>
2024-01-17 00:32:42 -0300

    Fix error swiper

---
+++ src/store/experienceStore.ts
@@ -0,0 +1,7 @@
+/* import {atom} from 'nanostores'
+
+export const experiences = atom([]);
+
+export function addExperience(experience){
+    experiences.set([...experiences.get(), experience])
+} */
